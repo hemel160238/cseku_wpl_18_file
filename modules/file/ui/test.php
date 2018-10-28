7fca56245450152631b152163ebe708c7935dd19
Eeshan <eeshan.islam@gmail.com>
2018-10-28 20:54:49 +0600

    Add File Forwarding

--- modules/file/ui/test.php
+++ modules/file/ui/test.php
@@ -3,13 +3,17 @@
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     $fileID = $_GET['fileid'];
-    $conn = mysqli_connect("localhost", "root", "", "cseku");
+    $userId = $_GET['userId'];
+    $conn = mysqli_connect("localhost", "root", "", "cseku2");
 
 
 
-    $sql = "SELECT fms_file.ID, ums_user.FirstName, ums_user.LastName, fms_file.Link, ums_discipline.Name, fms_file.Comment, fms_file.Timestamp
-FROM fms_file,ums_user,ums_discipline
-WHERE fms_file.ID = '$fileID' AND ums_user.ID = fms_file.CreatorID AND fms_file.CreatorID = ums_user.ID AND ums_user.DisciplineID = ums_discipline.ID;";
+    $sql = "SELECT * 
+FROM fms_fileassign,fms_file,ums_user,
+							(SELECT ums_user.ID AS FileSender,ums_discipline.Name AS DispName
+							FROM ums_discipline,ums_user
+							WHERE ums_discipline.ID = ums_user.DisciplineID) AS T
+WHERE fms_fileassign.ID = '$fileID' AND fms_file.CurrentUser = '$userId' AND fms_fileassign.sender = T.FileSender AND ums_user.ID = fms_fileassign.sender";
 
 
 
